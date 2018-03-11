033c2697c0b457161a228aab64e93a7a0914686f
Jeff DuBois <jeff.adubois@gmail.com>
2018-03-10 22:34:41 -0600

    initial push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big C', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Manistique Magnificent', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Build Your Own', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', false);
